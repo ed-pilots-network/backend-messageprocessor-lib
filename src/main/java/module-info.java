088e226b939aedbcbef46ead54d7ca7e1f169c0f
Pieter Van Eeckhout <wn5987@vandevelde.eu>
2023-06-27 12:23:18 +0200

    #1 making module names uniform

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
-module io.edpn.backend.messageprocessor.lib {
+module edpn.backend.messageprocessor.lib {
     requires lombok;
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.databind;
-    requires io.edpn.backend.util;
+    requires edpn.backend.util;
 
     exports io.edpn.backend.messageprocessorlib.application.dto.eddn;
     exports io.edpn.backend.messageprocessorlib.infrastructure.kafka.processor;
