11ca873bc3260c4471224f5b1cefdc927f86e8e0
Pieter Van Eeckhout <pieter.vaneeckhout@hoakt.com>
2023-08-06 21:55:25 +0200

    use Module enum as requesting module param

--- src/main/java/io/edpn/backend/messageprocessorlib/application/dto/eddn/data/StationDataRequest.java
+++ src/main/java/io/edpn/backend/messageprocessorlib/application/dto/eddn/data/StationDataRequest.java
@@ -1,9 +1,10 @@
 package io.edpn.backend.messageprocessorlib.application.dto.eddn.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.edpn.backend.util.Module;
 
 public record StationDataRequest(
-        @JsonProperty("requestingModule") String requestingModule,
+        @JsonProperty("requestingModule") Module requestingModule,
         @JsonProperty("stationName") String stationName,
         @JsonProperty("systemName") String systemName
 ) {}
--- src/main/java/io/edpn/backend/messageprocessorlib/application/dto/eddn/data/SystemDataRequest.java
+++ src/main/java/io/edpn/backend/messageprocessorlib/application/dto/eddn/data/SystemDataRequest.java
@@ -1,8 +1,9 @@
 package io.edpn.backend.messageprocessorlib.application.dto.eddn.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.edpn.backend.util.Module;
 
 public record SystemDataRequest(
-        @JsonProperty("requestingModule") String requestingModule,
+        @JsonProperty("requestingModule") Module requestingModule,
         @JsonProperty("systemName")String systemName
 ) {}
